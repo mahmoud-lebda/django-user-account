c1bab4a8e130dc0d2fbc303de07f000792880988
mahmoud <you@example.com>
2020-02-29 10:08:57 +0200

    add heroku

--- config/settings.py
+++ config/settings.py
@@ -129,18 +129,22 @@ USE_L10N = True
 
 USE_TZ = True
 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'config/static/')
+    os.path.join(PROJECT_ROOT, 'config/static/')
 
 ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # media files
